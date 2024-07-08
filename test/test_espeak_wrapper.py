70950b1c0ea1eaf4decba74d38c0bf722989eb52
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-07-08 19:16:22 +0900

    d: 유효하지 않은 테스트 제거

--- test/test_espeak_wrapper.py
+++ test/test_espeak_wrapper.py
@@ -18,7 +18,6 @@
 # pylint: disable=redefined-outer-name
 
 import os
-import pathlib
 import pickle
 import sys
 
@@ -35,7 +34,7 @@ def wrapper():
 
 def test_basic(wrapper):
     assert wrapper.version >= (1, 48)
-    assert 'espeak' in str(wrapper.library_path)
+    assert "espeak" in str(wrapper.library_path)
     assert os.path.isabs(wrapper.library_path)
     assert os.path.isabs(wrapper.data_path)  # not None, no raise
 
@@ -44,7 +43,7 @@ def test_available_voices(wrapper):
     espeak = set(wrapper.available_voices())
     assert espeak
 
-    mbrola = set(wrapper.available_voices('mbrola'))
+    mbrola = set(wrapper.available_voices("mbrola"))
     # can be empty if no mbrola voice installed (occurs only on Windows, at
     # least within the github CI pipeline)
     if mbrola:
@@ -54,31 +53,33 @@ def test_available_voices(wrapper):
 def test_set_get_voice(wrapper):
     assert wrapper.voice is None
     with pytest.raises(RuntimeError) as err:
-        wrapper.set_voice('')
+        wrapper.set_voice("")
     assert 'invalid voice code ""' in str(err)
 
-    wrapper.set_voice('fr-fr')
-    assert wrapper.voice.language == 'fr-fr'
+    wrapper.set_voice("fr-fr")
+    assert wrapper.voice.language == "fr-fr"
     assert wrapper.voice.name in (
-        'French (France)',  # >1.48.3
-        'french')           # older espeak
+        "French (France)",  # >1.48.3
+        "french",
+    )  # older espeak
 
-    wrapper.set_voice('en-us')
-    assert wrapper.voice.language == 'en-us'
+    wrapper.set_voice("en-us")
+    assert wrapper.voice.language == "en-us"
     assert wrapper.voice.name in (
-        'English (America)',  # >1.48.3
-        'english-us')         # older espeak
+        "English (America)",  # >1.48.3
+        "english-us",
+    )  # older espeak
 
     # no mbrola voices available on Windows by default (at least on the github
     # CI pipeline)
-    if sys.platform != 'win32':
-        wrapper.set_voice('mb-af1')
-        assert wrapper.voice.language == 'af'
-        assert wrapper.voice.name == 'afrikaans-mbrola-1'
+    if sys.platform != "win32":
+        wrapper.set_voice("mb-af1")
+        assert wrapper.voice.language == "af"
+        assert wrapper.voice.name == "afrikaans-mbrola-1"
 
     with pytest.raises(RuntimeError) as err:
-        wrapper.set_voice('some non existant voice code')
-    assert 'invalid voice code' in str(err)
+        wrapper.set_voice("some non existant voice code")
+    assert "invalid voice code" in str(err)
 
 
 def _test_pickle(voice):
@@ -96,15 +97,16 @@ def _test_pickle(voice):
 
 
 def test_pickle_en_us():
-    _test_pickle('en-us')
+    _test_pickle("en-us")
 
 
 @pytest.mark.skipif(
-    not EspeakMbrolaBackend.is_available() or
-    not EspeakMbrolaBackend.is_supported_language('mb-fr1'),
-    reason='mbrola or mb-fr1 voice not installed')
+    not EspeakMbrolaBackend.is_available()
+    or not EspeakMbrolaBackend.is_supported_language("mb-fr1"),
+    reason="mbrola or mb-fr1 voice not installed",
+)
 def test_pickle_mb_fr1():
-    _test_pickle('mb-fr1')
+    _test_pickle("mb-fr1")
 
 
 def test_twice():
@@ -115,20 +117,11 @@ def test_twice():
     assert wrapper1.version == wrapper2.version
     assert wrapper1.library_path == wrapper2.library_path
 
-    wrapper1.set_voice('fr-fr')
-    assert wrapper1.voice.language == 'fr-fr'
-    wrapper2.set_voice('en-us')
-    assert wrapper2.voice.language == 'en-us'
-    assert wrapper1.voice.language == 'fr-fr'
+    wrapper1.set_voice("fr-fr")
+    assert wrapper1.voice.language == "fr-fr"
+    wrapper2.set_voice("en-us")
+    assert wrapper2.voice.language == "en-us"
+    assert wrapper1.voice.language == "fr-fr"
 
     # pylint: disable=protected-access
     assert wrapper1._espeak._tempdir != wrapper2._espeak._tempdir
-
-
-@pytest.mark.skipif(sys.platform == 'win32', reason='not supported on Windows')
-def test_deletion():
-    # pylint: disable=protected-access
-    wrapper = EspeakWrapper()
-    path = pathlib.Path(wrapper._espeak._tempdir)
-    del wrapper
-    assert not path.exists()
