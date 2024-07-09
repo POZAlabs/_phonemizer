f5a7f69121304d6bd91ff10e2987eccf0f959d11
Sunwoong KIM <35526032+novdov@users.noreply.github.com>
2024-07-09 16:58:02 +0900

    u: phonemize 노출 누락 오류 수정

--- phonemizer/__init__.py
+++ phonemizer/__init__.py
@@ -12,3 +12,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with phonologizer. If not, see <http://www.gnu.org/licenses/>.
+
+from . import phonemize
+
+__all__ = ["phonemize"]
